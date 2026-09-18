a6bf1c1ce0cbb01c5a251a16a25c2af7a72046d6
ionenspiegel <topcuyusuf254@gmail.com>
2026-09-18 23:22:42 +0300

    İletişim V2

--- ga4.js
+++ ga4.js
@@ -1,7 +1,9 @@
 // İonenSpiegel Google Analytics 4
-// Measurement ID: G-PVPMSV7J1F
-(function() {
+// Ölçüm Kimliği: G-PVPMSV7J1F
+
+(function () {
   var id = "G-PVPMSV7J1F";
+
   if (!document.querySelector('script[data-ionen-ga4]')) {
     var s = document.createElement('script');
     s.async = true;
@@ -9,10 +11,17 @@
     s.dataset.ionenGa4 = '1';
     document.head.appendChild(s);
   }
+
   window.dataLayer = window.dataLayer || [];
-  function gtag() { window.dataLayer.push(arguments); }
+
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
   window.gtag = window.gtag || gtag;
+
   gtag('js', new Date());
+
   gtag('config', id, {
     anonymize_ip: true,
     page_title: document.title,
