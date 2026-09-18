b1dc51ae77f636145fe28fccf64e84f7383bc0f0
ionenspiegel <topcuyusuf254@gmail.com>
2026-09-18 12:19:17 +0300

    V43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // İonenSpiegel güncel service worker
-const CACHE_NAME = 'ionenspiegel-v42';
+const CACHE_NAME = 'ionenspiegel-v43';
 
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
