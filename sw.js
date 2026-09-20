114dc054b831855a3eb68bee77093e0b4817ea0b
ionenspiegel <topcuyusuf254@gmail.com>
2026-09-20 10:58:29 +0300

    20 eylül v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // İonenSpiegel V62 service worker
-const CACHE_NAME='ionenspiegel-v67-daily-20260920';
+const CACHE_NAME='ionenspiegel-v20-09-2026-67-daily-20260920';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
