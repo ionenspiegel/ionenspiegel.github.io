4dabd316a90eea348ec83f1aad8e78111790b1c4
ionenspiegel <topcuyusuf254@gmail.com>
2026-09-20 22:42:51 +0300

    V70

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // İonenSpiegel V62 service worker
-const CACHE_NAME='ionenspiegel-v20-09-2026-v69';
+const CACHE_NAME='ionenspiegel-v70-contact-20-09-2026';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
